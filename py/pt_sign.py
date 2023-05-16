d9c21bb1e8be8a7ec1eb3efd74bca6528bea442e
okko123 <katkit1135@outlook.com>
2023-05-16 10:03:30 +0800

    Create pt_sign.py
    pt自动签到脚本

---
+++ py/pt_sign.py
@@ -0,0 +1,54 @@
+import logging
+import os
+import requests
+import sys
+
+pt_url = "xxxxxxxx"
+
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+    "Cache-Control": "max-age=0",
+    "Connection": "keep-alive",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.33",
+    "Content-Type": "application/x-www-form-urlencoded",
+}
+
+
+def create_logger(application, verbose=None, logfile=None):
+  if verbose:
+    lowestseverity = logging.DEBUG
+  else:
+    lowestseverity = logging.INFO
+
+  logger = logging.getLogger(application)
+  logger.setLevel(lowestseverity)
+
+  ch = logging.StreamHandler()
+  ch.setLevel(lowestseverity)
+
+  formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+  ch.setFormatter(formatter)
+
+  logger.addHandler(ch)
+
+  if logfile:
+    fh = logging.FileHandler(logfile)
+    fh.setLevel(lowestseverity)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+  return logger
+
+logger = create_logger(os.path.basename(sys.argv[0]), "False", "/data/tools/pt.log")
+
+with open("/data/tools/cookies_soulvoice.txt") as f:
+    cookies = dict([l.split("=", 1) for l in f.read().split(";")])
+
+try:
+    r = requests.get(url=pt_url, headers=headers, cookies=cookies, timeout=5)
+    logger.info("OK")
+except requests.exceptions.Timeout as e:
+    logger.info("timeout %s" % e)
